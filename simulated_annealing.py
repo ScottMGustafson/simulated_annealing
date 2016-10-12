2109a332e61e695cbce3f35454e24b4b6637fb11
astroscott <s1gustaf@gmail.com>
2016-10-12 14:04:42 -0700

    ...

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -36,7 +36,7 @@ def simulated_annealing(fn, *x, **kwargs):
     Tmin=float(kwargs.pop("Tmin",.0001))
     alpha=float(kwargs.pop("alpha",0.9))
     T=float(kwargs.pop("Tstart",10000.))
-
+    epsilon=float(kwargs.pop("epsilon",10e-8)) #tolerance multiplier to define locked params
     max_steps=int(kwargs.pop("max_steps",1000))
     chi2_lim=int(kwargs.pop("chi2_lim",4))
     param_ranges=kwargs
@@ -53,8 +53,13 @@ def simulated_annealing(fn, *x, **kwargs):
             for i in range(max_steps):
                 new_guess={}
                 for key, val in guess.items():
-                    step_size=_step*np.fabs(param_ranges[key][1]-param_ranges[key][0])
-                    new_guess[key] = guess[key] + np.random.normal(scale=np.fabs(step_size))
+                    #if a param_range shows that the item is locked, don't vary it
+                    _rng=np.fabs(param_ranges[key][1]-param_ranges[key][0])
+                    if _rng<epsilon:
+                        new_guess[key]=guess[key]
+                    else:
+                        step_size=_step*_rng
+                        new_guess[key] = guess[key] + np.random.normal(scale=np.fabs(step_size))
                 new_chi2=chi2(fn(x, **new_guess), y,ye)
                 if accept_prob(old_chi2, new_chi2,T)>np.random.uniform():
                     old_chi2=new_chi2
@@ -69,13 +74,13 @@ def simulated_annealing(fn, *x, **kwargs):
                 #if no convergence, restart by reinitializing T
                 T=old_chi2
                 guess=get_random_samples(1, **param_ranges)
-                if new_chi2<10.*min_chi2:
+                if new_chi2<min_chi2+10.*chi2_lim:
                     #append if chi2 is reasonably small
                     samples.append( [guess, new_chi2] )
 
             #since we are saving each model tried, get rid of absurdly poor models
             min_chi2=min([it[-1] for it in samples])
-            samples=[it for it in samples if it[-1]<10.*min_chi2]
+            samples=[it for it in samples if it[-1]<min_chi2+10.*chi2_lim]
 
             T*=alpha
     except KeyboardInterrupt:
@@ -94,18 +99,18 @@ def param_locked_SA(fn, *x,  **kwargs):
     function to run simulated annealing with locking parameters of interest
     (see Avni 1976)
     """
-    num_steps=int(kwargs.get("num_steps",10))
-    max_steps=int(kwargs.get("max_steps",400))
+    num_steps=int(kwargs.get("num_steps",10)) #num of locked values to test/param
+    max_steps=int(kwargs.get("max_steps",400)) #max num iterations for
     chi2_lim=int(kwargs.get("chi2_lim",4))
 
     param_ranges={k:v for k,v in kwargs.iteritems() if not k in "max_steps chi2_lim".split()}
     param_samples, all_chi2=None,None
     epsilon=1./float(num_steps)
     for param, rng in param_ranges.iteritems():
+        #lock each param
         for val in np.linspace(rng[0],rng[1], num_steps):
             #update the parameter and put into kwargs
-
-            new_rng=sorted([(1-epsilon)*val, (1+epsilon)*val])
+            new_rng=[val,val]
             kwargs[param]=new_rng
             psamp, chi2=simulated_annealing(fn, *x, **kwargs)
             if all_chi2 is None:
--- test.py
+++ test.py
@@ -9,13 +9,15 @@ b_true=43.
 c_true=6000.
 
 def generate_test_data(SNR=20.,a=a_true,b=b_true,c=c_true):
-    xx=np.random.uniform(-10.,10.,size=100)
+    """
+    generate a signal with poisson noise
+    
+    """
+    xx=np.random.uniform(-5.,5.,size=100)
     model=test_fn(xx,a,b,c)
-    _scale=np.fabs(np.median(model))/SNR,
-    noise=np.random.normal(scale=_scale, size=np.shape(xx)[0])
+    noise=np.fabs(model)*np.random.normal(scale=1./SNR, size=np.shape(xx)[0])  #sqrt(N) is poiss
     y=model+noise
-    ye=np.fabs(np.random.normal(scale=_scale, size=np.shape(xx)[0]))
-    return xx, y, ye
+    return xx, y, noise
 
 def test_fn(x,a=a_true,b=b_true,c=c_true):
     return a*x**2+b*x+c
@@ -23,25 +25,25 @@ def test_fn(x,a=a_true,b=b_true,c=c_true):
 if __name__=="__main__":
     x,y,ye=generate_test_data()
     param_samples, out =param_locked_SA(test_fn,x,y,ye,
-                                            a=[-15.,-8.], 
-                                            b=[40.,50.], 
-                                            c=[400.,750.])
+                                            a=[-13.,-11.], 
+                                            b=[40.,45.], 
+                                            c=[5900.,6100.])
     #param_samples, out =simulated_annealing(test_fn,x,y,ye,
     #                                        a=[-15.,-8.], 
     #                                        b=[40.,50.], 
     #                                        c=[400.,750.])
-
 
     plt.plot(x,y,'ko')
     xx=np.arange(np.amin(x),np.amax(x), 0.01)
     plt.plot(xx, test_fn(xx,**param_samples[0]), 'r-')
     plt.show()
 
-    x=[it['a'] for it in param_samples.tolist()]
-    plt.plot(x,out,'ko')
-    plt.xlabel('a')
-    plt.ylabel(r'$\chi^2$')
-    plt.show()
+    for attr in "a b c".split():
+        x=[it['a'] for it in param_samples.tolist()]
+        plt.plot(x,out,'ko')
+        plt.xlabel(attr)
+        plt.ylabel(r'$\chi^2$')
+        plt.show()
 
 
     out=np.array([it for it in out.tolist() if it<out[0]+4.])
@@ -55,8 +57,7 @@ if __name__=="__main__":
         _max=max(lst)
         _min=max(lst)
         best=param_samples[0][param]
-        print(best, _max-best, best-_min)
+        print param, best, "+",_max-best, best-_min
+    print "chi2/dof=",np.amin(out)/float((np.shape(y)[0]-3.))
 
-   
-
-    
+  
